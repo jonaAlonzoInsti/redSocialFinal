d3e88fe77beb0d855e5c21a11a0e247636cef66a
jonaAlonzoInsti <149473406+jonaAlonzoInsti@users.noreply.github.com>
2024-02-28 20:38:19 +0100

    Update build.sh

--- render/build.sh
+++ render/build.sh
@@ -1,8 +1,9 @@
 set -o errexit -o nounset -o pipefail
 pip install -r requirements.txt
 # db-migrate -e prod up
-python manage.py tailwind start
+# python manage.py tailwind start
 npm install -g rimraf
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py tailwind build
+
+# python manage.py tailwind build
