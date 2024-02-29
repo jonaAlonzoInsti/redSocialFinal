71d5a64150910b71afab902e5319c354c39b4115
jonaAlonzoInsti <149473406+jonaAlonzoInsti@users.noreply.github.com>
2024-02-29 10:22:24 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -210,7 +210,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP= os.path.join(BASE_DIR, 'static')
 
 
