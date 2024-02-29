f33b66a11eec9def921031cab6f7201dcd55dc88
jonaAlonzoInsti <149473406+jonaAlonzoInsti@users.noreply.github.com>
2024-02-29 11:13:03 +0100

    Update build.sh

--- render/build.sh
+++ render/build.sh
@@ -5,7 +5,7 @@ npm install -g tailwindcss
 # db-migrate -e prod up
 # python manage.py tailwind start
 npm install -g rimraf
-npx tailwindcss  -i ./static/src/input.css -o ./static/src/output.css
+# npx tailwindcss  -i ./static/src/input.css -o ./static/src/output.css
 python manage.py collectstatic --noinput
 python manage.py migrate
 
