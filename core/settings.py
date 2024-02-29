3d1ba4441a171830d18721e49a7ff78062cf2333
jonaAlonzoInsti <149473406+jonaAlonzoInsti@users.noreply.github.com>
2024-02-29 09:46:32 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -291,4 +291,4 @@ if not DEBUG:
 
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
